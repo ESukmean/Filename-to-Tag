e2a8766371658705f04264a7c4398e3cb217d155
ESukmean <esukmean@esukmean.com>
2016-04-23 18:18:24 +0900

    support ID3v1 Tag

--- main.py
+++ main.py
@@ -1,35 +1,57 @@
 import sys
 import os, os.path
 
-def check_mp3ver(path):
-	pass
+ENCODING = 'utf-8'
 
-def set_id3v1(path, title):
-	pass
+def check_mp3ver(file):
+	to_return = 1;
+	
+	if file.read(3) == b'ID3':
+		to_return = to_return * 32
+	
+	file.seek(-128, 2)
+	if file.read(3) == b'TAG':
+		to_return = to_return * 31
+	
+	return to_return
+	
+def set_id3v1(file, title):
 
-def set_id3v2(path, title):
-	pass
+	file.seek(-125, 2)
+	
+	to_write = bytes(title, ENCODING)
+	to_write += bytes((0 for x in range(30 - len(to_write))))
+	
+	file.write(to_write)
+
+def set_id3v2(file, title):
+	return False
 
 def proc(path):
 	if path[-4:] != '.mp3':
-		print(path, 'is not MP3 file. skip')
 	
-	ver = check_mp3ver(path)
-	if ver == 31:
-		pass
-	elif ver == 32:
-		pass
-	
-	pass
+	title = os.path.basename(path)[:-4]
+	file = open(path, 'r+b')
+	ver = check_mp3ver(file)
 
+        
+	if ver == -1:
+		return False
+	
+	if ver % 31 == 0:
+		set_id3v1(file, title)
+	
+	elif ver % 32 == 0:
+		set_id3v2(file, title)		
+		
+	return False
 
 for path in sys.argv[1:]:
 	if os.path.isfile(path):
-		proc(path)
 	
 	elif os.path.isdir(path):
 		for fpath in os.walk(path):
 			for fname in fpath:
 				proc(fname)
 	else:
-		print('<', path, '> not Exists')
+		
